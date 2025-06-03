05db6849ec28f57ccfbc45a0e8e617a4490e05ce
Matias <matias.costamagna@mi.unc.edu.ar>
2025-06-03 15:04:07 -0300

    Cambio de fondo e implementacion sonido accion incorrecta

--- src/view/PyramidView.java
+++ src/view/PyramidView.java
@@ -9,14 +9,19 @@ public class PyramidView {
     private final JFrame frame = new JFrame("Pyramid Card Game");
     private final JPanel pyramidPanel = new JPanel();
     private final JPanel rightPanel = new JPanel();
-    private final JButton auxDeckButton = new JButton("Mazo Auxiliar");
     private final JButton activeCardButton = new JButton("Carta activa");
     private static PyramidView instance;
+    private final JButton auxDeckButton = crearMazoAuxiliarButton();
+    private final Dimension cardDimension = new Dimension(60, 100);
 
     private PyramidView() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLayout(new BorderLayout());
+
+        ImageIcon bgIcon = new ImageIcon(("assets/fondo5.jpg"));
+        JPanel backgroundPanel = new BackgroundPanel(bgIcon.getImage());
+        frame.setContentPane(backgroundPanel);
 
         pyramidPanel.setLayout(new BoxLayout(pyramidPanel, BoxLayout.Y_AXIS));
         frame.add(pyramidPanel, BorderLayout.CENTER);
@@ -25,9 +30,19 @@ public class PyramidView {
         rightPanel.add(Box.createVerticalGlue());
         rightPanel.add(auxDeckButton);
         rightPanel.add(Box.createVerticalStrut(10));
+        activeCardButton.setPreferredSize(cardDimension);
+        activeCardButton.setMaximumSize(cardDimension);
         rightPanel.add(activeCardButton);
         rightPanel.add(Box.createVerticalGlue());
+        auxDeckButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        activeCardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         frame.add(rightPanel, BorderLayout.EAST);
+
+        pyramidPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        pyramidPanel.setOpaque(false);
+        rightPanel.setOpaque(false);
+
 
         activeCardButton.setVisible(false);
         frame.setVisible(true);
@@ -54,15 +69,21 @@ public class PyramidView {
     }
 
     public JButton createCardButton(Card card) {
-        JButton button = new JButton(card.toString());
-        button.setPreferredSize(new Dimension(60, 100));
+        JButton button = new JButton(formatCardLabel(card));
+        button.setPreferredSize(cardDimension);
         return button;
     }
 
-    public void resetCardButton(JButton button) {
-        button.setText("");
-        button.setEnabled(false);
-        button.setBackground(null);
+    public String formatCardLabel(Card card) {
+        return "<html><center>" + card.getValue() + "<br>de<br>" + card.getSuit() + "</center></html>";
+    }
+
+    private JButton crearMazoAuxiliarButton() {
+        String label = "<html><center>Mazo<br>Auxiliar</center></html>";
+        JButton button = new JButton(label);
+        button.setPreferredSize(new Dimension(120, 40));
+        button.setOpaque(false);
+        return button;
     }
 
     public static PyramidView getInstance() {
